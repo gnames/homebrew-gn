af107015a5d02a115ea3dbf1c210efba88392f7f
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-01-25 17:40:15 -0600

    fix hash

--- Formula/gnverify.rb
+++ Formula/gnverify.rb
@@ -2,7 +2,7 @@ class Gnverify < Formula
   desc "Verifies scientific names via exact and fuzzy matching."
   homepage "https://github.com/gnames/gnverify"
   url "https://github.com/gnames/gnverify/archive/v0.1.0.tar.gz"
-  sha256 "f55cf40cdb47b8612c2edfba3e6f5a16bce5e9495f34d9858bf536ef052cf525"
+  sha256 "5f60c6cc2c41bd4968b2ea046e2da8a0a07031ae20bb9634b08f90ecf0611757"
   license "MIT"
 
   depends_on "go" => :build
