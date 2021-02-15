ada8cab41cb9f467033169de0bfa5ff0cb71601e
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-02-15 11:40:25 -0600

    update sha for gnfiles

--- Formula/gnfiles.rb
+++ Formula/gnfiles.rb
@@ -2,7 +2,7 @@ class Gnfiles < Formula
   desc "Uses IPFS to exchange files between computers"
   homepage "https://github.com/gnames/gnfiles"
   url "https://github.com/gnames/gnfiles/archive/v0.0.1.tar.gz"
-  sha256 "58f529a46604992a0e9af9f81b19269d0493113fa136acfe77411afe93e8322d"
+  sha256 "54aed0bfd6f0d27e6b58b50270ea6a136910669a798dd5a8204b706d1506207c"
   license "MIT"
 
   depends_on "go" => :build
