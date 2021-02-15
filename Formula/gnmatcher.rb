02f7186f88e4b04bf1784d23217524c70503fbab
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-02-15 11:49:20 -0600

    typo

--- Formula/gnmatcher.rb
+++ Formula/gnmatcher.rb
@@ -1,7 +1,7 @@
 class Gnmatcher < Formula
   desc "Takes putative scientific names and matches them known names"
   homepage "https://github.com/gnames/gnmatcher"
-  url "https://github.com/gnames/gnfiles/archive/v0.5.5.tar.gz"
+  url "https://github.com/gnames/gnmatcher/archive/v0.5.5.tar.gz"
   sha256 "54aed0bfd6f0d27e6b58b50270ea6a136910669a798dd5a8204b706d1506207c"
   license "MIT"
 
