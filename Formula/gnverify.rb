f085ed0036da8cf29c496e6fcec43a69e55df2a3
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-03-14 14:13:34 -0500

    update gnverify to v0.2.1

--- Formula/gnverify.rb
+++ Formula/gnverify.rb
@@ -1,7 +1,7 @@
 class Gnverify < Formula
   desc "Verifies scientific names via exact and fuzzy matching"
   homepage "https://github.com/gnames/gnverify"
-  url "https://github.com/gnames/gnverify/archive/v0.1.0.tar.gz"
+  url "https://github.com/gnames/gnverify/archive/v0.2.1.tar.gz"
   sha256 "5f60c6cc2c41bd4968b2ea046e2da8a0a07031ae20bb9634b08f90ecf0611757"
   license "MIT"
 
