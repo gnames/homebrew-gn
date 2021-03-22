7042b8fafaa75047953906e8bb418aa8d3eefa03
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-03-22 18:28:23 -0500

    gnparser v1.1.0

--- Formula/gnparser.rb
+++ Formula/gnparser.rb
@@ -1,19 +1,19 @@
 class Gnparser < Formula
-  desc "Breaks biodiversity scientific names into their elements"
-  homepage "https://github.com/gnames/gnparser"
-  url "https://github.com/gnames/gnparser/archive/v1.0.12.tar.gz"
-  sha256 "bb9a1a5271d3361dc545531bce9901eb03dbd2f3771020267267f553fc4aadcb"
-  license "MIT"
+  desc 'Breaks biodiversity scientific names into their elements'
+  homepage 'https://github.com/gnames/gnparser'
+  url 'https://github.com/gnames/gnparser/archive/v1.1.0.tar.gz'
+  sha256 'bb9a1a5271d3361dc545531bce9901eb03dbd2f3771020267267f553fc4aadcb'
+  license 'MIT'
 
-  depends_on "go" => :build
+  depends_on 'go' => :build
 
   def install
-    man1.install "man/gnparser.1"
-    system "go", "build", "-o", *std_go_args, "./gnparser"
+    man1.install 'man/gnparser.1'
+    system 'go', 'build', '-o', *std_go_args, './gnparser'
   end
 
   test do
     res = `#{bin}/gnparser -f compact "Bubo bubo L."`
-    assert_match /"simple":"Bubo bubo"/, res
+    assert_match(/"simple":"Bubo bubo"/, res)
   end
 end
