5a390a0cf040f60a8f1bdef80db662425392ba0b
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-06-18 19:45:05 -0500

    add gnfinder

---
+++ Formula/gnfinder.rb
@@ -0,0 +1,18 @@
+class Gnfinder < Formula
+  desc 'Finds scientific names in texts, PDF files, MS Word documents etc.'
+  homepage 'https://github.com/gnames/gnfinder'
+  url 'https://github.com/gnames/gnfinder/archive/v0.14.0.tar.gz'
+  sha256 '35c9f7aa34a3fed45de2542c3b500dc3faaa5b7f256c17279daecf0e5dd55476'
+  license 'MIT'
+
+  depends_on 'go' => :build
+
+  def install
+    system 'go', 'build', '-o', *std_go_args, './gnfinder'
+  end
+
+  test do
+    res = `echo "Bubo bubo" | #{bin}/gnfinder -f compact"`
+    assert_match(/"name":"Bubo bubo"/, res)
+  end
+end
