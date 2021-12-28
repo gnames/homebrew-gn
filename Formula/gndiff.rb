5a543237680295e5ea11be5339871a68747e5de6
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-12-28 11:11:12 -0600

    gndiff sha

--- Formula/gndiff.rb
+++ Formula/gndiff.rb
@@ -2,7 +2,7 @@ class Gndiff < Formula
   desc 'Compares two files with scientific names, no internet connection required.'
   homepage 'https://github.com/gnames/gndiff'
   url 'https://github.com/gnames/gndiff/archive/v0.1.1.tar.gz'
-  sha256 '0b4cb2906efd8f7def763048b68a0f206ac35ee237030d716e1f566408d2ef34'
+  sha256 '7db8ec1a41a9b493abd71f3a2cef231c86739c98391bf4f8461864b6ef40ebc9'
   license 'MIT'
 
   depends_on 'go' => :build
