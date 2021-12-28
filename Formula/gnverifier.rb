162d19ef7e8eac0bb05fff06a3ad683cb27ac912
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-12-28 11:35:17 -0600

    gnverifier sha

--- Formula/gnverifier.rb
+++ Formula/gnverifier.rb
@@ -2,7 +2,7 @@ class Gnverifier < Formula
   desc 'Verifies scientific names via exact and fuzzy matching'
   homepage 'https://github.com/gnames/gnverifier'
   url 'https://github.com/gnames/gnverifier/archive/v0.6.4.tar.gz'
-  sha256 '7cb1cb049264c4e25b9f96a50d7355f9a5aea50ede8f7aba49da735b689e102a'
+  sha256 '79fb657cffcd4c23b1dc1ddcaedb1e114616c87bb8240a7ebfd43cc4fd1f64e5'
   license 'MIT'
 
   depends_on 'go' => :build
