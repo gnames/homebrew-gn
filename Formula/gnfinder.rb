db8d9cb73e520986ef7210f51a4cc1805ceded06
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-02-23 17:44:44 +0000

    fix gnfinder build

--- Formula/gnfinder.rb
+++ Formula/gnfinder.rb
@@ -8,7 +8,7 @@ class Gnfinder < Formula
   depends_on 'go' => :build
 
   def install
-    system 'go', 'build', '-o', *std_go_args, './gnfinder'
+    system 'go', 'build', '-o', *std_go_args, '.'
   end
 
   test do
