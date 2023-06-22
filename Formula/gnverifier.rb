db7ad36c7688a85b3aaae4e9f9c00baad22092df
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-06-22 01:57:41 +0000

    gnverifier v1.1.1

--- Formula/gnverifier.rb
+++ Formula/gnverifier.rb
@@ -1,7 +1,7 @@
 class Gnverifier < Formula
   desc 'Verifies scientific names via exact and fuzzy matching'
   homepage 'https://github.com/gnames/gnverifier'
-  url 'https://github.com/gnames/gnverifier/archive/v1.0.2.tar.gz'
+  url 'https://github.com/gnames/gnverifier/archive/v1.1.1.tar.gz'
   sha256 '1fe3a63792245a2021fafa8d6b574bcbc8e281984082e0fec2e46db44f025e0d'
   license 'MIT'
 
