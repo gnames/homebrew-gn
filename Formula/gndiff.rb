426dcba9bbd54d6c4b3c997ffd596c74d092172b
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-09-06 19:09:41 +0000

    gndiff v0.2.1

--- Formula/gndiff.rb
+++ Formula/gndiff.rb
@@ -1,7 +1,7 @@
 class Gndiff < Formula
   desc 'Compares two files with scientific names, no internet connection required.'
   homepage 'https://github.com/gnames/gndiff'
-  url 'https://github.com/gnames/gndiff/archive/v0.1.1.tar.gz'
+  url 'https://github.com/gnames/gndiff/archive/v0.2.1.tar.gz'
   sha256 '7db8ec1a41a9b493abd71f3a2cef231c86739c98391bf4f8461864b6ef40ebc9'
   license 'MIT'
 
