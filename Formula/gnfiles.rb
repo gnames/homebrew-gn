a3f8e51d46ee494c6d7847090e331a35aec7f84b
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-02-02 18:07:00 +0000

    sha gnfinder

--- Formula/gnfiles.rb
+++ Formula/gnfiles.rb
@@ -2,7 +2,7 @@ class Gnfiles < Formula
   desc "Uses IPFS to exchange files between computers"
   homepage "https://github.com/gnames/gnfiles"
   url "https://github.com/gnames/gnfiles/archive/v0.0.1.tar.gz"
-  sha256 "54aed0bfd6f0d27e6b58b50270ea6a136910669a798dd5a8204b706d1506207c"
+  sha256 "1d310a7dea8661628c19d97c2dd53fc119e35fc963dca823dcee437b8cea1967"
   license "MIT"
 
   depends_on "go" => :build
