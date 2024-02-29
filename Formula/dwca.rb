b7f6722fb310c8f5b3769d8e428a013d1ee70f4f
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-02-29 20:26:15 +0000

    add dwca v0.1.0

---
+++ Formula/dwca.rb
@@ -0,0 +1,18 @@
+class DwCA < Formula
+  desc "A command line tool to work with Darwin Core Archives"
+  homepage "https://github.com/gnames/dwca"
+  url "https://github.com/gnames/dwca/archive/v0.1.0.tar.gz"
+  sha256 ""
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", *std_go_args, "./gnfiles"
+  end
+
+  test do
+    res = `#{bin}/dwca -V`
+    assert_match /version/, res
+  end
+end
