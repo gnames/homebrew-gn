cb01bb7ac4c34739bc8bbfa1fa2efa8b5e23dc83
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-05-24 00:25:21 +0000

    sha for gnfinder

--- Formula/gnfinder.rb
+++ Formula/gnfinder.rb
@@ -2,7 +2,7 @@ class Gnfinder < Formula
   desc 'Finds scientific names in texts, PDF files, MS Word documents etc.'
   homepage 'https://github.com/gnames/gnfinder'
   url 'https://github.com/gnames/gnfinder/archive/v1.1.5.tar.gz'
-  sha256 '1d310a7dea8661628c19d97c2dd53fc119e35fc963dca823dcee437b8cea1967'
+  sha256 'caa5355cac9a6c8b819b5e0672fe9d8c14f04fd20372684151021f3fc8de0748'
   license 'MIT'
 
   depends_on 'go' => :build
