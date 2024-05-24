0fd61c5110b1ab1dc9cf0585c39bef50aaf158ad
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-05-24 00:22:21 +0000

    gnfinder v1.1.5

--- Formula/gnfinder.rb
+++ Formula/gnfinder.rb
@@ -1,7 +1,7 @@
 class Gnfinder < Formula
   desc 'Finds scientific names in texts, PDF files, MS Word documents etc.'
   homepage 'https://github.com/gnames/gnfinder'
-  url 'https://github.com/gnames/gnfinder/archive/v1.1.4.tar.gz'
+  url 'https://github.com/gnames/gnfinder/archive/v1.1.5.tar.gz'
   sha256 '1d310a7dea8661628c19d97c2dd53fc119e35fc963dca823dcee437b8cea1967'
   license 'MIT'
 
