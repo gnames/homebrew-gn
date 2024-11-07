9253f89d91570ca7dfebbb483bdcb6204d3bfad3
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-11-07 12:31:32 +0000

    gnparser v1.10.4

--- Formula/gnparser.rb
+++ Formula/gnparser.rb
@@ -1,8 +1,8 @@
 class Gnparser < Formula
   desc 'Breaks biodiversity scientific names into their elements'
   homepage 'https://github.com/gnames/gnparser'
-  url 'https://github.com/gnames/gnparser/archive/v1.10.3.tar.gz'
-  sha256 '899661ab88b716b7c1e3c4430bfb388997e510b28961e0246c361407f34cf0d2'
+  url 'https://github.com/gnames/gnparser/archive/v1.10.4.tar.gz'
+  sha256 ''
   license 'MIT'
 
   depends_on 'go' => :build
